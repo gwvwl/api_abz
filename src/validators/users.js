bdee04652fed4794ed739ba71811e0a0a72245dd
Valerii <97701282+gwvwl@users.noreply.github.com>
2023-04-14 15:58:30 +0200

    first

---
+++ src/validators/users.js
@@ -0,0 +1,20 @@
+const Joi = require("joi");
+const validatorHandler = require("../middlewares/validatorHandler");
+
+const signup = (req, res, next) => {
+  const schema = Joi.object().keys({
+    name: Joi.string().trim().min(3).max(50).required(),
+    email: Joi.string().trim().email().required(),
+    phone: Joi.string()
+      // .length(10)
+      // .pattern(/^[0-9]+$/)
+      .required(),
+    position: Joi.string().required(),
+    photo: Joi.object().unknown(true),
+  });
+  validatorHandler(req, res, next, schema);
+};
+
+module.exports = {
+  signup,
+};
