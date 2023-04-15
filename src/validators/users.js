0265e4510c4e614cf119e66cbdbd97c77eb0c687
Valerii <97701282+gwvwl@users.noreply.github.com>
2023-04-15 16:55:32 +0200

    fix validate email

--- src/validators/users.js
+++ src/validators/users.js
@@ -4,7 +4,10 @@ const validatorHandler = require("../middlewares/validatorHandler");
 const signup = (req, res, next) => {
   const schema = Joi.object().keys({
     name: Joi.string().trim().min(3).max(50).required(),
-    email: Joi.string().trim().email().required(),
+    email: Joi.string()
+      .trim()
+      .email({ tlds: { allow: false } })
+      .required(),
     phone: Joi.string()
       // .length(10)
       // .pattern(/^[0-9]+$/)
